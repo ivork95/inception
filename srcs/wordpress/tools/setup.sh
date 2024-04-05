714d934a45d9501dd76bbf352885ac8ba8aa4b8a
ivork95 <ivork95@github.com>
2024-04-05 16:10:30 +0200

    wordpress setup

--- srcs/wordpress/tools/setup.sh
+++ srcs/wordpress/tools/setup.sh
@@ -7,17 +7,35 @@ done
 
 if [ ! -f "/var/www/wp-config.php" ]; then
 echo "Creating config"
-wp-cli config create --allow-root --path=/var/www --dbhost="$WP_DB_HOST"\
-	--dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD"
+wp-cli config create\
+	--allow-root\
+	--path=/var/www\
+	--dbhost="$MYSQL_HOST"\
+	--dbname="$MYSQL_DATABASE"\
+	--dbuser="$MYSQL_USER"\
+	--dbpass="$MYSQL_PASSWORD"
 
 echo "Running wp core install"
 wp-cli core install\
-	--allow-root --path=/var/www --skip-email --title="$WP_ADM_TITLE" --admin_name="$WP_ADM_NAME"\
-	--admin_password="$WP_ADM_PASSWORD" --admin_email="$WP_ADM_MAIL" --url="$WP_ADM_URL"
+	--allow-root\
+	--path=/var/www\
+	--skip-email\
+	--title="$WP_ADM_TITLE"\
+	--admin_name="$WP_ADM_NAME"\
+	--admin_password="$WP_ADM_PASS"\
+	--admin_email="$WP_ADM_MAIL"\
+	--url="$WP_ADM_URL"
 
 echo "Running wp user create"
-wp-cli user create "$WP_USER_NAME" "$WP_USER_MAIL" --allow-root --path=/var/www --role=author --user_pass="$WP_USER_PASS"
+wp-cli user create\
+	"$WP_USER_NAME"\
+	"$WP_USER_MAIL"\
+	--allow-root\
+	--path=/var/www\
+	--role=contributor\
+	--user_pass="$WP_USER_PASS"
 fi
 
 echo "Running php-fpm"
+# --nodaemonize forces staying in the foreground
 php-fpm7.4 --nodaemonize
